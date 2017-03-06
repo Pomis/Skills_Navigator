49719a66d583dc31fdc424bd9d8b5c0583f79b40
Pomis <pomis172@gmail.com>
2017-03-06 17:00:26 +0300

    регистрация и прочее

--- routes/reg.js
+++ routes/reg.js
@@ -1,15 +1,57 @@
-var express = require('express');
-var router = express.Router();
+'use strict'
 
-/* Авторизация */
+const express = require('express');
+const router = express.Router();
+const nodemailer = require('nodemailer');
+
+
+/* Регистрация */
 router.post('/', (req, res, next) => {
-  let login = req.params.login
-  let passhash = req.params.passhash
-  let mail = req.params.mail
-  let confirmed = req.params.confirmed
+  let name = req.body.name
+  let passhash = req.body.passhash
+  let mail = req.body.mail
 
+  global.db.query("SELECT COUNT(*) FROM Users WHERE Users.mail = ?", [mail])
 
-  res.send(200, {access_token: "kek"})
+  	.then(count => {
+  		console.log("count: "+count[0]['COUNT(*)'])
+  		if (count[0]['COUNT(*)']>0)
+  			throw "Пользователь с таким email уже зарегистрирован"
+  		return count[0]['COUNT(*)']==0
+  	})
+
+  	.then(canInsert => {
+  		if (canInsert){
+	  		global.db.query("INSERT INTO Users SET ?", {
+		  		name: name,
+		  		mail: mail,
+		  		confirmed: false,
+		  		passhash: passhash,
+		  	})
+	  	}
+	})
+
+  	.then(ok => {
+  		if (ok) {
+  			let mailOptions = {
+			    from: '"Superb bot 👻" <snavibot@gmail.com>', // sender address
+			    to: mail, // list of receivers
+			    subject: 'Hello ✔', // Subject line
+			    text: 'Hello world ?', // plain text body
+			    html: '<b>Hello world ?</b>' // html body
+			}
+			transporter.sendMail(mailOptions)
+  		}
+  	})
+
+  	.then(() => res.send(200, {access_token: "kek"}))
+
+  	.catch(err => {
+  		console.log(err)
+  		res.send(400, {error: err})
+  	})
+
+  //res.send(200, {access_token: "kek"})
 });
 
 module.exports = router;
